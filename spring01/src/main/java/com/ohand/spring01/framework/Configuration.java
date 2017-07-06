0380a780dc25bae3c8feb0c58ab865c34d45a9e0
a154832918 <a154832918@163.com>
2017-07-06 20:00:07 +0800

    地方

--- spring01/src/main/java/com/ohand/spring01/framework/Configuration.java
+++
@@ -1,9 +0,0 @@
-package com.ohand.spring01.framework;
-
-public interface Configuration {
-	interface Status {
-		int STATUS_OK = 1;
-		int STATUS_FAIL = 2;
-		int STATUS_INVALID_TOKEN = 3;
-	}
-}
--- spring01/src/main/java/com/ohand/spring01/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package com.ohand.spring01.service;
-
-import com.ohand.spring01.domain.PersonDomain;
-
-public interface PersonService {
-	
-	public PersonDomain getObjectVOById(Integer id);
-	
-}
